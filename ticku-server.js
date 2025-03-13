6f89fa2fb9902abf5576179c46d8e882455e6b3e
zangwoo Jeon <create7827@naver.com>
2025-03-13 17:14:29 +0900

    feat: 배당 페이지 API 연결

--- ticku-server.js
+++ ticku-server.js
@@ -32,6 +32,8 @@ app.use("/img", express.static(path.join(__dirname, "public/img")));
     const companyInfoRouter = require("./routes/companyInfo");
     const yearResultRouter = require("./routes/yearResult");
     const volumeRouter = require("./routes/volumes");
+    const dividendRouter = require("./routes/dividend");
+
     // 라우트 등록
     app.use("/auth", authRouter);
     app.use("/user", usersRouter);
@@ -44,6 +46,7 @@ app.use("/img", express.static(path.join(__dirname, "public/img")));
     app.use("/companyInfo", companyInfoRouter);
     app.use("/yearResult", yearResultRouter);
     app.use("/volumes", volumeRouter);
+    app.use("/dividend", dividendRouter);
 
     //서버 실행
     app.listen(PORT, () => {
