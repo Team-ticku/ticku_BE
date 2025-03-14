3ecf4add495e77aad40f70d42fbfb9e96fd03786
zangwoo Jeon <create7827@naver.com>
2025-03-14 17:30:46 +0900

    feat: 뉴스 스크랩 저장 및 삭제

--- models/ScrapNews.js
+++ models/ScrapNews.js
@@ -2,20 +2,21 @@ const mongoose = require("mongoose");
 
 const ScrapNewsSchema = new mongoose.Schema(
   {
-    title: String,
-    content: String,
-    hasImage: Boolean,
-    image: String,
-    sourceName: String,
-    sourceImage: String,
-    defaultBookmarked: Boolean,
-    userId: String,
+    title: { type: String, required: true },
+    link: { type: String, required: true }, // 링크 추가
+    pubDate: { type: Date, required: true }, // 발행일 (Date 타입)
+    content: String, // 필수는 아님
+    hasImage: Boolean, // 필수는 아님
+    image: String, // 필수는 아님
+    sourceName: { type: String, required: true },
+    sourceImage: String, // 필수는 아님
+    userId: { type: String, required: true },
   },
   { collection: "scrap_news" }
 );
 
-// 계정 DB
-const ScrapNews = mongoose.model("ScrapNews", ScrapNewsSchema);
+// userId와 link 조합에 대한 unique 제약 조건 (선택 사항)
+ScrapNewsSchema.index({ userId: 1, link: 1 }, { unique: true, sparse: true });
 
-// User 모델 외부로 내보내기기
+const ScrapNews = mongoose.model("ScrapNews", ScrapNewsSchema);
 module.exports = ScrapNews;
